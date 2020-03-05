e8aaf9f516b894546197dfd64d7ebcebf680b866
TexnoDgo <texnodgo@gmail.com>
2020-03-05 13:02:10 +0200

    create core and base.html

--- ORP_site/OR/OR/settings.py
+++ ORP_site/OR/OR/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'OR.core',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'OR.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'OR/templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
