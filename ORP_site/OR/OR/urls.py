b855b8b29f469252f8159dd9d588d9ab9e98cb98
TexnoDgo <texnodgo@gmail.com>
2020-03-06 11:02:13 +0200

    add HomePage

--- ORP_site/OR/OR/urls.py
+++ ORP_site/OR/OR/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from HomePage.views import index
 
 urlpatterns = [
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
 ]
